2f41a1366a395ae2b666af824041d52bf17875ed
Dylan Avery <fossilfuelmusic@gmail.com>
2017-04-14 08:49:14 -0600

    make it better

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.locals.title = 'Garage Bin'
 
 
 app.get('/', (request, response) => {
-  response.sendFile("/build/" + "index.html")
+  response.sendFile( __dirname + "/build/" + "index.html" )
 })
 
 app.get('/api/v1/items/', (request, response) => {
